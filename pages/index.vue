4158d9d1656a46b334d253b665399584104c9ffc
Luciano Lima <luciano.lima93@outlook.com.br>
2023-08-26 16:36:16 -0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Bem vindo ao Nuxt</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-teal-500">Produto {{ id }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const { id } = useRoute().params;
+
+    return {
+      id
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
